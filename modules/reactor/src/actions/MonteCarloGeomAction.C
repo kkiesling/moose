8e4ba05d23bbf7e738d1479e5927240593b2e8ae
Kalin Kiesling <krkiesling@gmail.com>
2023-07-17 13:32:47 -0500

    generate assembly mesh region

--- modules/reactor/src/actions/MonteCarloGeomAction.C
+++ modules/reactor/src/actions/MonteCarloGeomAction.C
@@ -75,20 +75,91 @@ MonteCarloGeomAction::act()
 void
 MonteCarloGeomAction::makeCoreMeshJSON(std::string mesh_generator_name)
 {
-  const auto & mg = _app.getMeshGenerator(mesh_generator_name);
+  //const auto & mg = _app.getMeshGenerator(mesh_generator_name);
 }
 
 void
 MonteCarloGeomAction::makeAssemblyMeshJSON(std::string mesh_generator_name)
 {
-  const auto & mg = _app.getMeshGenerator(mesh_generator_name);
+  //const auto & mg = _app.getMeshGenerator(mesh_generator_name);
+  nlohmann::json titan_inp;
+
+  const auto is_single_pin = getMeshProperty<bool>(RGMB::is_single_pin, mesh_generator_name);
+
+  if  (!is_single_pin)
+  {
+    // get pin lattice
+    const auto pin_lattice =
+        getMeshProperty<std::vector<std::vector<int>>>(RGMB::pin_lattice, mesh_generator_name);
+    const auto pin_names =
+        getMeshProperty<std::vector<std::string>>(RGMB::pin_names, mesh_generator_name);
+
+    // check if square or hex lattice and get dimension
+    std::string lat_type;
+    int dimension;
+    std::string dim_type;
+    if (pin_lattice[0].size() == pin_lattice[1].size())
+    {
+      lat_type = "SQUARE_MAP";
+      dimension = pin_lattice[0].size();
+      dim_type = "dim";
+    }
+    else
+    {
+      lat_type = "HEX_MAP";
+      dimension = pin_lattice.size() / 2 + 1; // integer division
+      dim_type = "num_rings";
+    }
+
+    // convert list of ints to corresponding list of pin names
+    std::vector<std::vector<std::string>> elements;
+    for (auto & pin_list : pin_lattice)
+    {
+      std::vector<std::string> ele_list;  // ring or row element fills
+
+      for (auto & pin_id : pin_list)
+      {
+        std::string pin_name = pin_names[pin_id];
+        ele_list.push_back(pin_name);
+      }
+      elements.push_back(ele_list);
+    }
+
+    titan_inp[mesh_generator_name + "_lattice"] = {
+      lat_type,
+      {
+        {dim_type, dimension},
+        {"pitch", 1.0},  // placeholder
+        {"fill", "mat"}, // placeholder
+        {"elements", elements}
+      }
+    };
+
+    // get any duct regions
+    if (hasMeshProperty(RGMB::duct_halfpitches, mesh_generator_name))
+    {
+      int duct_id = 0;
+      const auto duct_halfpitches = getMeshProperty<std::vector<Real>>(RGMB::duct_halfpitches, mesh_generator_name);
+
+      for (auto & hp : duct_halfpitches)
+      {
+        // create a polygon with the appropriate inserts
+      }
+    }
+
+    // create the actual assembly which is the combo of any lattice and duct regions
+    // name for the full assembly is mesh_generator_name with no suffixes
+
+    Moose::out << titan_inp.dump(4) << std::endl;
+
+  }
+
 }
 
 void
 MonteCarloGeomAction::makePinMeshJSON(std::string mesh_generator_name)
 {
-  const auto & mg = _app.getMeshGenerator(mesh_generator_name);
-
+  //const auto & mg = _app.getMeshGenerator(mesh_generator_name);
   nlohmann::json titan_inp;
 
   // get the ring radii and create the pin/material list
